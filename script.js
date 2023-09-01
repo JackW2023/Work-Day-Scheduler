bf4d513952ec8402c08a019740cbda8df2265c81
JW <daxon2012@gmail.com>
2023-08-31 17:15:13 -0700

    Added Dates and time

--- script.js
+++ script.js
@@ -39,6 +39,16 @@ $(function () {
         return '5PM';
     }
   }
+
+
+  const date = document.getElementById("currentDay")
+  date.textContent = new Date().toLocaleDateString('en-US', {
+    weekday: "long",
+    month: "long",
+    day: "numeric",
+    year: "numeric"
+
+  })
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
